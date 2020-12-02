ddd30eff700f76dfb4beb2fbeec10666752068c6
Christopher <cmimms@salesforce.com>
2020-12-02 11:36:17 -0500

    Added 2 problem.

--- 1/1.go
+++ 1/1.go
@@ -10,6 +10,7 @@ import (
 func main() {
 	lines, _ := readLinesToInt("1_input.txt")
 
+	star1(lines)
 	star2(lines)
 }
 
@@ -26,10 +27,8 @@ func star1(lines []int) {
 
 func star2(lines []int) {
 	m := make(map[int]bool)
-	for _, num := range lines {
-		m[num] = true
-	}
 	for i, num := range lines {
+		m[num] = true
 		for _, num2 := range lines[i+1:] {
 			cor := 2020 - (num + num2)
 			if m[cor] == true {
@@ -39,8 +38,6 @@ func star2(lines []int) {
 	}
 }
 
-// readLines reads a whole file into memory
-// and returns a slice of its lines.[
 func readLinesToInt(path string) ([]int, error) {
 	file, err := os.Open(path)
 	if err != nil {
