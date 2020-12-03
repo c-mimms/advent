33dfa5fe2c715c5da38563f7d5594d32b8c83d99
Christopher <cmimms@salesforce.com>
2020-12-02 20:51:09 -0500

    Finished day 2

--- 2/2.go
+++ 2/2.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
 	"strings"
 )
 
@@ -11,17 +12,31 @@ func main() {
 	lines, _ := readLines("2_input.txt")
 
 	//Count correct passwords
+	count := 0
 	for _, line := range lines {
-		// password := line[1]
+
+		//Needs to be an easier way to do ths
+		password := line[1]
 		rules := strings.Split(line[0], " ")
-		fmt.Println(rules[0])
+		ruleRange := strings.Split(rules[0], "-")
+		low, _ := strconv.Atoi(ruleRange[0])
+		high, _ := strconv.Atoi(ruleRange[1])
+		letter := rules[1]
+
+		//Star 1
+		// letterCount := strings.Count(password, letter)
+		// if letterCount >= low && letterCount <= high {
+		// 	count += 1
+		// }
+
+		//Star 2
+		if (password[low] == letter[0]) != (password[high] == letter[0]) {
+			count += 1
+		}
+
 	}
+	fmt.Println(count)
 
-}
-
-//4-5 c: ccssnccccc
-type passwordRecord struct {
-	rule, password string
 }
 
 func readLines(path string) ([][]string, error) {
@@ -35,9 +50,14 @@ func readLines(path string) ([][]string, error) {
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		str := scanner.Text()
-		// num, _ := strconv.Atoi(scanner.Text())
 		lines = append(lines, strings.Split(str, ":"))
 
 	}
 	return lines, scanner.Err()
 }
+
+//Didn't end up using this
+//4-5 c: ccssnccccc
+// type passwordRecord struct {
+// 	rule, password string
+// }
